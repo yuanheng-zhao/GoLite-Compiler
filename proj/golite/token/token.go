e8eb0063bf99321bfb155aa5ced0673e6946715e
Ian An <qan001@uchicago.edu>
2021-10-29 19:11:21 -0500

    print token type alongside token literal

--- proj/golite/token/token.go
+++ proj/golite/token/token.go
@@ -8,64 +8,64 @@ type TokenType string
 
 const (
 	ILLEGAL = "ILLEGAL"
-	EOF     = "eof"
-	INT     = "int"
-	NUM     = "number" // an integer containing one or more digits (0-9)
-	BOOL    = "bool"
-	TRUE    = "true"
-	FALSE   = "false"
-	ID      = "id"
-	NIL     = "nil"
+	EOF     = "EOF"
+	INT     = "INT"
+	NUM     = "NUM" // an integer containing one or more digits (0-9)
+	BOOL    = "BOOL"
+	TRUE    = "TRUE"
+	FALSE   = "FALSE"
+	ID      = "ID"
+	NIL     = "NIL"
 
-	LET     = "let"
-	PRINT   = "Print"
-	PRINTLN = "Println"
-	RETURN  = "return"
-	PACK    = "package"
-	IMPORT  = "import"
-	FMT     = "fmt"
-	TYPE    = "type"
-	STRUCT  = "struct"
-	SCAN    = "Scan"
-	IF      = "if"
-	ELSE    = "else"
-	FOR     = "for"
-	FUNC    = "func"
-	VAR     = "var"
+	LET     = "LET"
+	PRINT   = "PRINT"
+	PRINTLN = "PRINTLN"
+	RETURN  = "RETURN"
+	PACK    = "PACK"
+	IMPORT  = "IMPORT"
+	FMT     = "FMT"
+	TYPE    = "TYPE"
+	STRUCT  = "STRUCT"
+	SCAN    = "SCAN"
+	IF      = "IF"
+	ELSE    = "ELSE"
+	FOR     = "FOR"
+	FUNC    = "FUNC"
+	VAR     = "VAR"
 
-	DOT     = "."
-	COMMA   = ","
-	QTDMARK = "\""
-	LBRACE  = "{"
-	RBRACE  = "}"
-	LPAREN  = "("
-	RPAREN  = ")"
+	DOT     = "DOT"
+	COMMA   = "COMMA"
+	QTDMARK = "QTDMARK"
+	LBRACE  = "LBRACE"
+	RBRACE  = "RBRACE"
+	LPAREN  = "LPAREN"
+	RPAREN  = "RPAREN"
 
-	ASSIGN    = "="
-	AMPERS    = "&" // for getting address
-	SEMICOLON = ";"
-	ADD       = "+"
-	MINUS     = "-"
-	MULTIPLY  = "*"
-	DIVIDE    = "/"
-	OR        = "||"
-	AND       = "&&"
-	NOT       = "!"
-	EQUAL     = "=="
-	NEQUAL    = "!="
-	GT        = ">"
-	GE        = ">="
-	LT        = "<"
-	LE        = "<="
-	COMMENT   = "//"
+	ASSIGN    = "ASSIGN"
+	AMPERS    = "AMPERS" // for getting address
+	SEMICOLON = "SEMICOLON"
+	ADD       = "ADD"
+	MINUS     = "MINUS"
+	MULTIPLY  = "MULTIPLY"
+	DIVIDE    = "DIVIDE"
+	OR        = "OR"
+	AND       = "AND"
+	NOT       = "NOT"
+	EQUAL     = "EQUAL"
+	NEQUAL    = "NEQUAL"
+	GT        = "GT"
+	GE        = "GE"
+	LT        = "LT"
+	LE        = "LE"
+	COMMENT   = "COMMENT"
 )
 
 type Token struct {
-	Type        TokenType
-	Literal     string
-	type_lookup string
+	Type    TokenType
+	Literal string
+	//type_lookup string
 }
 
 func (tok Token) String() string {
-	return fmt.Sprintf("%T, %v", tok.Type, tok.Literal)
+	return fmt.Sprintf("Token.%v(%v)", tok.Type, tok.Literal)
 }
