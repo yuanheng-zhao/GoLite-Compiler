4e10e5314bb0e003b9591386105faeacc68411a0
Ian An <qan001@uchicago.edu>
2021-11-22 15:54:38 -0600

    Add one test case for parser

--- proj/golite/parser/parser_test.go
+++ proj/golite/parser/parser_test.go
@@ -1,1 +1,35 @@
 package parser
+
+import (
+	"bufio"
+	"os"
+	"proj/golite/scanner"
+	ct "proj/golite/token"
+	"testing"
+)
+
+func Test1(t *testing.T) {
+
+	f_obj, _ := os.Open("test1_parser.golite")
+	reader := bufio.NewReader(f_obj)
+	myScanner := scanner.New(reader)
+	// The expected result struct represents the token stream for the input source
+	tokens := []ct.Token{
+		{ct.LPAREN, "(", 1},
+		{ct.ID, "a", 1},
+		{ct.OR, "||", 1},
+		{ct.ID, "b", 1},
+		{ct.AND, "&&", 1},
+		{ct.ID, "c", 1},
+		{ct.EQUAL, "==", 1},
+		{ct.TRUE, "true", 1},
+		{ct.RPAREN, ")", 1},
+	}
+
+	// Define  a new scanner for some Cal program
+	parser := New(*myScanner)
+	ast := parser.Parse()
+	if ast == nil {
+		t.Errorf("\nParse(%v)\nExpected:%v\nGot:%v", tokens, "Valid AST", nil)
+	}
+}
