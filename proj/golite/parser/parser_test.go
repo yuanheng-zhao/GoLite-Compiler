0e5598d981de8a4aa4faa2e53cc3139712f208b7
Zhaoyh-Jonathan <jonathan.zhaoyh@gmail.com>
2021-11-23 12:02:39 -0600

    remove invalid tests, add tests for parser

--- proj/golite/parser/parser_test.go
+++ proj/golite/parser/parser_test.go
@@ -10,96 +10,8 @@ import (
 )
 
 func Test1(t *testing.T) {
-
-	f_obj, _ := os.Open("test1_parser.golite")
-	reader := bufio.NewReader(f_obj)
-	myScanner := scanner.New(reader)
-	// The expected result struct represents the token stream for the input source
-	tokens := []ct.Token{
-		{ct.LPAREN, "(", 1},
-		{ct.ID, "a", 1},
-		{ct.OR, "||", 1},
-		{ct.ID, "b", 1},
-		{ct.AND, "&&", 1},
-		{ct.ID, "c", 1},
-		{ct.EQUAL, "==", 1},
-		{ct.TRUE, "true", 1},
-		{ct.RPAREN, ")", 1},
-	}
-
-	// Define  a new scanner for some Cal program
-	parser := New(*myScanner)
-	ast := parser.Parse()
-	if ast == nil {
-		t.Errorf("\nParse(%v)\nExpected:%v\nGot:%v", tokens, "Valid AST", nil)
-	}
-
-	fmt.Println("AST Printout:", ast.String())
-}
-
-func Test2(t *testing.T) {
-
-	f_obj, _ := os.Open("test2_parser.golite")
-	reader := bufio.NewReader(f_obj)
-	myScanner := scanner.New(reader)
-	// The expected result struct represents the token stream for the input source
-	tokens := []ct.Token{
-		{ct.LPAREN, "(", 1},
-		{ct.ID, "a", 1},
-		{ct.OR, "||", 1},
-		{ct.ID, "b", 1},
-		{ct.AND, "&&", 1},
-		{ct.ID, "c", 1},
-		{ct.EQUAL, "==", 1},
-		{ct.TRUE, "true", 1},
-	}
-
-	// Define  a new scanner for some Cal program
-	parser := New(*myScanner)
-	ast := parser.Parse()
-	if ast != nil {
-		t.Errorf("\nParse(%v)\nExpected:%v\nGot:%v", tokens, "nil", "Valid AST")
-	}
-}
-
-func Test3(t *testing.T) {
-
-	f_obj, _ := os.Open("test3_parser.golite")
-	reader := bufio.NewReader(f_obj)
-	myScanner := scanner.New(reader)
-	// The expected result struct represents the token stream for the input source
-	tokens := []ct.Token{
-		{ct.ID, "apple", 1},
-		{ct.LPAREN, "(", 1},
-		{ct.ID, "banana", 1},
-		{ct.COMMA, ",", 1},
-		{ct.ID, "orange", 1},
-		{ct.COMMA, ",", 1},
-		{ct.ID, "apple", 1},
-		{ct.LPAREN, "(", 1},
-		{ct.INT, "1", 1},
-		{ct.COMMA, ",", 1},
-		{ct.INT, "2", 1},
-		{ct.COMMA, ",", 1},
-		{ct.INT, "66", 1},
-		{ct.RPAREN, ")", 1},
-		{ct.RPAREN, ")", 1},
-	}
-
-	// Define  a new scanner for some Cal program
-	parser := New(*myScanner)
-	ast := parser.Parse()
-	if ast == nil {
-		t.Errorf("\nParse(%v)\nExpected:%v\nGot:%v", tokens, "Valid AST", nil)
-	}
-
-	//fmt.Println("AST Printout:\n", ast.String())
-}
-
-func Test4(t *testing.T) {
-
-	f_obj, _ := os.Open("test4_parser.golite")
-	reader := bufio.NewReader(f_obj)
+	fileObj, _ := os.Open("test1_parser.golite")
+	reader := bufio.NewReader(fileObj)
 	myScanner := scanner.New(reader)
 	// The expected result struct represents the token stream for the input source
 	tokens := []ct.Token{
@@ -116,27 +28,97 @@ func Test4(t *testing.T) {
 		{ct.FUNC, "func", 3},
 		{ct.ID, "main", 3},
 		{ct.LPAREN, "(", 3},
-		{ct.ID, "a", 3},
-		{ct.INT, "int", 3},
 		{ct.RPAREN, ")", 3},
 		{ct.LBRACE, "{", 3},
 
+		{ct.VAR, "var", 4},
 		{ct.ID, "a", 4},
-		{ct.ASSIGN, "=", 4},
-		{ct.NUM, "1", 4},
-		{ct.ADD, "+", 4},
-		{ct.NUM, "1", 4},
+		{ct.INT, "int", 4},
 		{ct.SEMICOLON, ";", 4},
 
-		{ct.RBRACE, "}", 4},
+		{ct.ID, "a", 5},
+		{ct.ASSIGN, "=", 5},
+		{ct.NUM, "1", 5},
+		{ct.ADD, "+", 5},
+		{ct.NUM, "1", 5},
+		{ct.SEMICOLON, ";", 5},
+
+		{ct.RBRACE, "}", 6},
 	}
 
-	// Define  a new scanner for some Cal program
 	parser := New(*myScanner)
 	ast := parser.Parse()
 	if ast == nil {
 		t.Errorf("\nParse(%v)\nExpected:%v\nGot:%v", tokens, "Valid AST", nil)
 	}
 
-	//fmt.Println("AST Printout:\n", ast.String())
+	fmt.Println("AST Printout:")
+	fmt.Println(ast.String())
+}
+
+func Test2(t *testing.T) {
+
+	fileObj, _ := os.Open("test2_parser.golite")
+	reader := bufio.NewReader(fileObj)
+	myScanner := scanner.New(reader)
+	// The expected result struct represents the token stream for the input source
+	tokens := []ct.Token{
+		{ct.PACK, "package", 1},
+		{ct.ID, "main", 1},
+		{ct.SEMICOLON, ";", 1},
+
+		{ct.IMPORT, "import", 2},
+		{ct.QTDMARK, "\"", 2},
+		{ct.FMT, "fmt", 2},
+		{ct.QTDMARK, "\"", 2},
+		{ct.SEMICOLON, ";", 2},
+
+		{ct.FUNC, "func", 3},
+		{ct.ID, "main", 3},
+		{ct.LPAREN, "(", 3},
+		{ct.RPAREN, ")", 3},
+		{ct.LBRACE, "{", 3},
+
+		{ct.VAR, "var", 4},
+		{ct.ID, "b", 4},
+		{ct.INT, "bool", 4},
+		{ct.SEMICOLON, ";", 4},
+
+		{ct.ID, "b", 5},
+		{ct.ASSIGN, "=", 5},
+		{ct.TRUE, "true", 5},
+		{ct.OR, "||", 5},
+		{ct.FALSE, "false", 5},
+		{ct.AND, "&&", 5},
+		{ct.LPAREN, "(", 5},
+		{ct.TRUE, "true", 5},
+		{ct.EQUAL, "==", 5},
+		{ct.TRUE, "true", 5},
+		{ct.RPAREN, ")", 5},
+		{ct.SEMICOLON, ";", 5},
+
+		{ct.RBRACE, "}", 6},
+	}
+
+	parser := New(*myScanner)
+	ast := parser.Parse()
+	if ast == nil {
+		t.Errorf("\nParse(%v)\nExpected:%v\nGot:%v", tokens, "Valid AST", nil)
+	}
+	fmt.Println("AST Printout:")
+	fmt.Println(ast.String())
+}
+
+func Test3(t *testing.T) {
+	fileObj, _ := os.Open("test3_parser.golite")
+	reader := bufio.NewReader(fileObj)
+	myScanner := scanner.New(reader)
+
+	parser := New(*myScanner)
+	ast := parser.Parse()
+	if ast == nil {
+		t.Errorf("\nParse(%v)\nExpected:%v\nGot:%v", "Ignore (Too long)", "Valid AST", nil)
+	}
+
+	fmt.Println(ast.String())
 }
