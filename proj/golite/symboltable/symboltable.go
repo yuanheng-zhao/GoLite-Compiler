e772587b96559e974c7cdce3fef440e00c1d62d8
Ian An <qan001@uchicago.edu>
2021-11-24 00:50:36 -0600

    Function signatures for semantic analysis, empty bodies

--- proj/golite/symboltable/symboltable.go
+++ proj/golite/symboltable/symboltable.go
@@ -1,1 +1,59 @@
 package symboltable
+
+import "proj/golite/types"
+
+type SymbolTable struct {
+	Parent *SymbolTable
+	htable map[string] *Entry
+	ScopeName string
+	ScopeParamTy []types.Type
+}
+
+func New (parent *SymbolTable, scopeName string, scopeParamTy []types.Type) *SymbolTable {
+	return &SymbolTable{parent, make(map[string] *Entry), scopeName, scopeParamTy}
+}
+
+func (st *SymbolTable) Contains(tokLiteral string) Entry {
+	if entry, exists := st.htable[tokLiteral]; exists {
+		// token literal exists in the symbol table
+		return *entry
+	}
+	return nil
+}
+
+func (st *SymbolTable) Insert(tokLiteral string, entry *Entry) {
+	st.htable[tokLiteral] = entry
+}
+
+
+
+
+
+type Entry interface {
+
+}
+
+type VarEntry struct {
+
+}
+
+func NewVarEntry() *VarEntry {
+	return &VarEntry{}
+}
+
+type FuncEntry struct {
+
+}
+
+func NewFuncEntry() *FuncEntry {
+	return &FuncEntry{}
+}
+
+type StructEntry struct {
+
+}
+
+func NewStructEntry() *StructEntry {
+	return &StructEntry{}
+}
+
--- proj/golite/types/type.go
+++ proj/golite/types/type.go
@@ -1,1 +1,58 @@
 package types
+
+type Type interface {
+	GetName() string
+}
+
+type IntTy struct {}
+
+func (intTy *IntTy) GetName() string {
+	return "int"
+}
+
+type BoolTy struct {}
+
+func (boolTy *BoolTy) GetName() string {
+	return "bool"
+}
+
+type FuncTy struct {}
+
+func (funcTy *FuncTy) GetName() string {
+	return "function"
+}
+
+type StructTy struct {}
+
+func (structTy *StructTy) GetName() string {
+	return "struct"
+}
+
+type UnknownTy struct{}
+
+func (unknownTy *UnknownTy) GetName() string {
+	return "unknownType"
+}
+
+type VoidTy struct {}
+
+func (voidTy *VoidTy) GetName() string {
+	return "void"
+}
+
+
+var IntTySig *IntTy
+var BoolTySig *BoolTy
+var FuncTySig *FuncTy
+var StructTySig *StructTy
+var UnknownTySig *UnknownTy
+var VoidTySig *VoidTy
+
+func init() {
+	IntTySig = &IntTy{}
+	BoolTySig = &BoolTy{}
+	FuncTySig = &FuncTy{}
+	StructTySig = &StructTy{}
+	UnknownTySig = &UnknownTy{}
+	VoidTySig = &VoidTy{}
+}
