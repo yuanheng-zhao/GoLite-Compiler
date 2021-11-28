f5c06fe9c3e50f38858397dd90e754c3d14dca0a
Zhaoyh-Jonathan <jonathan.zhaoyh@gmail.com>
2021-11-27 18:59:34 -0600

    remove old comments in golite.go

--- proj/golite/golite.go
+++ proj/golite/golite.go
@@ -4,11 +4,9 @@ import (
 	"flag"
 	"fmt"
 	"os"
-	"proj/golite/sa"
-
 	ct "proj/golite/context"
-	//"proj/golite/sa"
 	ps "proj/golite/parser"
+	"proj/golite/sa"
 	sc "proj/golite/scanner"
 )
 
@@ -22,38 +20,6 @@ func StartCompile(ctx ct.CompilerContext) {
 }
 
 func main() {
-	//IMPLEMENT ME!
-	//usageStatement := "To read in a Golite program and print out each token\n	Usage: go run golite.go -<FLAG> <FILENAME>"
-	//args := os.Args[1:]
-	//if len(args) != 2 {
-	//	fmt.Println(usageStatement)
-	//	os.Exit(0)
-	//}
-	//flag := s.TrimSpace(args[0])
-	//filename := s.TrimSpace(args[1])
-	//
-	//if flag == "-lex" {
-	//	f_obj, err := os.Open(filename)
-	//	if err != nil { // the filename should be valid
-	//		fmt.Fprintf(os.Stderr, "error: %v\n", err)
-	//		os.Exit(1)
-	//	}
-	//	reader := bufio.NewReader(f_obj)
-	//	my_scanner := scanner.New(reader)
-	//
-	//	// var tok token.TokenType
-	//	// for tok != token.EOF {
-	//	// 	tok = my_scanner.NextToken().Type
-	//	// 	fmt.Println(tok)
-	//	// }
-	//
-	//	var tok token.Token
-	//	// eof_token := token.Token{Type: token.EOF, Literal: "EOF"}
-	//	for tok.Type != token.EOF {
-	//		tok = my_scanner.NextToken()
-	//		fmt.Println(tok)
-	//	}
-	//}
 
 	// Define all optional flags for the compiler
 	lexOpt := flag.Bool("lex", false, "Send to standard-out the tokens from scanner.")
