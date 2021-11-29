0be654c2a463a3be2b30a024a912c8487ec05db3
Zhaoyh-Jonathan <jonathan.zhaoyh@gmail.com>
2021-11-28 23:19:37 -0600

    add helper functions to symboltable

--- proj/golite/symboltable/symboltable.go
+++ proj/golite/symboltable/symboltable.go
@@ -11,12 +11,15 @@ type SymbolTable struct {
 	ScopeName       string
 	ScopeParamTys   []types.Type
 	ScopeParamNames []string
+	//ProtoName 		string  // name of the prototype struct
 }
 
 func New(parent *SymbolTable, scopeName string) *SymbolTable {
+	//return &SymbolTable{parent, make(map[string]*Entry), scopeName, []types.Type{}, []string{}, ""}
 	return &SymbolTable{parent, make(map[string]*Entry), scopeName, []types.Type{}, []string{}}
 }
 
+// TO-DO : revise Contains
 func (st *SymbolTable) Contains(tokLiteral string) Entry {
 	if entry, exists := st.htable[tokLiteral]; exists {
 		// token literal exists in the symbol table
@@ -29,6 +32,63 @@ func (st *SymbolTable) Insert(tokLiteral string, entry *Entry) {
 	st.htable[tokLiteral] = entry
 }
 
+// PowerContains returns the entry of a variable searching all symbol tables at the current and above levels
+func (st *SymbolTable) PowerContains(varName string) Entry {
+	var entry Entry
+	currSymtable := st
+	for {
+		if entry = currSymtable.Contains(varName); entry == nil {
+			if currSymtable.Parent == nil {
+				return nil
+			} else {
+				currSymtable = currSymtable.Parent
+			}
+		} else {
+			break
+		}
+	}
+	return entry
+}
+
+// CheckGlobalVariable returns true if the given variable name is a global variable name, otherwise false
+func (st *SymbolTable) CheckGlobalVariable(varName string) bool {
+	currSymtable := st
+	for {
+		if currSymtable.ScopeName == "global" {
+			// in the global symbol table
+			if varEntry := currSymtable.Contains(varName); varEntry != nil {
+				// global variable varName
+				return true
+			} else {
+				return false
+			}
+		} else {
+			if currSymtable.Parent != nil {
+				currSymtable = currSymtable.Parent
+			} else {
+				return false
+			}
+		}
+	}
+}
+
+//func (st *SymbolTable) SetProtoName(name string) {
+//	st.ProtoName = name
+//}
+//
+//func (st *SymbolTable) GetProtoName() string {
+//	return st.ProtoName
+//}
+
+//func (st *SymbolTable) GetCopy(scopeName string, parentSt *SymbolTable) *SymbolTable {
+//	instanceSt := New(parentSt, scopeName)
+//	for key, entry := range st.htable {
+//		instanceSt.htable[key] = entry.
+//	}
+//	instanceSt.SetProtoName(st.ScopeName)
+//	return instanceSt
+//}
+
 type Entry interface {
 	SetType(t types.Type)
 	SetValue(s string)
@@ -36,6 +96,7 @@ type Entry interface {
 	GetScopeST() *SymbolTable
 	GetReturnTy() types.Type // Only implement for funcEntry
 	GetRegId() int
+	//GetCopy(parentSt *SymbolTable) *Entry
 }
 
 type VarEntry struct {
@@ -67,6 +128,10 @@ func (ve *VarEntry) GetRegId() int {
 	return ve.regId
 }
 
+//func (ve *VarEntry) GetCopy(parentSt *SymbolTable) *VarEntry {
+//	return &VarEntry{ve.ty, ve.value, ir.NewRegister()}
+//}
+
 type FuncEntry struct {
 	ty         types.Type
 	returnType types.Type // expected return type
@@ -89,6 +154,10 @@ func (fe *FuncEntry) GetReturnTy() types.Type {
 }
 func (fe *FuncEntry) GetRegId() int { return -1 }
 
+//func (fe *FuncEntry) GetCopy(parentSt *SymbolTable) *Entry {  // cannot copy a function when initialize a struct
+//	return nil
+//}
+
 type StructEntry struct {
 	ty      types.Type
 	scopeSt *SymbolTable
@@ -110,3 +179,7 @@ func (se *StructEntry) GetReturnTy() types.Type {
 	return types.UnknownTySig
 }
 func (se *StructEntry) GetRegId() int { return -1 }
+
+//func (se *StructEntry) GetCopy(parentSt *SymbolTable) *StructEntry {
+//	return &StructEntry{se.ty, se.scopeSt.GetCopy(se.scopeSt.ProtoName, parentSt)}
+//}
