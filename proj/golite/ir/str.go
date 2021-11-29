9c4ca71be9636a0db558c2844150ea0641647f67
Zhaoyh-Jonathan <jonathan.zhaoyh@gmail.com>
2021-11-28 22:04:29 -0600

    add @ for global var case in ldr and str.go

--- proj/golite/ir/str.go
+++ proj/golite/ir/str.go
@@ -27,7 +27,7 @@ func (instr *Str) GetSources() []int {
 	sources := []int{}
 	if instr.opty == REGISTER {
 		sources = append(sources, instr.sourceReg, instr.operand)
-	} else if instr.opty == IMMEDIATE || instr.opty == ONEOPERAND{
+	} else if instr.opty == IMMEDIATE || instr.opty == ONEOPERAND {
 		sources = append(sources, instr.sourceReg)
 	}
 	return sources
@@ -61,17 +61,17 @@ func (instr *Str) String() string {
 	if instr.opty == REGISTER {
 		sourceReg1 := fmt.Sprintf("r%v", instr.sourceReg)
 		sourceReg2 := fmt.Sprintf("r%v", instr.operand)
-		out.WriteString(fmt.Sprintf("str %s,%s,%s", targetReg,sourceReg1,sourceReg2))
+		out.WriteString(fmt.Sprintf("str %s,%s,%s", targetReg, sourceReg1, sourceReg2))
 	} else if instr.opty == IMMEDIATE {
 		sourceReg := fmt.Sprintf("r%v", instr.sourceReg)
 		operand2 := fmt.Sprintf("#%v", instr.operand)
-		out.WriteString(fmt.Sprintf("str %s,%s,%s", targetReg,sourceReg,operand2))
+		out.WriteString(fmt.Sprintf("str %s,%s,%s", targetReg, sourceReg, operand2))
 	} else if instr.opty == ONEOPERAND {
 		sourceReg := fmt.Sprintf("r%v", instr.sourceReg)
-		out.WriteString(fmt.Sprintf("str %s,%s", targetReg,sourceReg))
+		out.WriteString(fmt.Sprintf("str %s,%s", targetReg, sourceReg))
 	} else if instr.opty == GLOBALVAR {
-		globVarName := fmt.Sprintf("%v", instr.globalVar)
-		out.WriteString(fmt.Sprintf("str %s,%s", targetReg,globVarName))
+		globVarName := fmt.Sprintf("@%v", instr.globalVar)
+		out.WriteString(fmt.Sprintf("str %s,%s", targetReg, globVarName))
 	}
 
 	return out.String()
