8a1ddf5ab6aef998e4e8d598c5c05fd62728d9f8
Zhaoyh-Jonathan <jonathan.zhaoyh@gmail.com>
2021-12-01 00:14:49 -0600

    add iloc tests

--- proj/golite/iloc/iloc_test.go
+++ proj/golite/iloc/iloc_test.go
@@ -59,3 +59,28 @@ func Test2(t *testing.T) {
 		}
 	}
 }
+
+func Test3(t *testing.T) {
+	ctx := ct.New(false, false, false, "test3_iloc.golite")
+	myScanner := scanner.New(*ctx)
+	myParser := parser.New(*myScanner)
+	ast := myParser.Parse()
+	//fmt.Println("AST Printout:")
+	//fmt.Println(ast.String())
+
+	globalSymTable := sa.PerformSA(ast)
+	if globalSymTable == nil {
+		t.Errorf("\nExpected: returned symbol table; Got nil\n")
+	}
+
+	globalFuncFrag := ast.TranslateToILocFunc([]*ir.FuncFrag{}, globalSymTable)
+	if globalFuncFrag == nil {
+		t.Errorf("\nExpected: returned FuncFrag; Got nil\n")
+	}
+	for _, funcFrag := range globalFuncFrag {
+		instructions := funcFrag.Body
+		for _, instruction := range instructions {
+			fmt.Println(instruction.String())
+		}
+	}
+}
