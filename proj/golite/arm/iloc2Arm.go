983d96168c1db8e25f9e2720981cf540bf768b77
Zhaoyh-Jonathan <jonathan.zhaoyh@gmail.com>
2021-12-08 13:26:03 -0600

    add var2offset dict

--- proj/golite/arm/iloc2Arm.go
+++ proj/golite/arm/iloc2Arm.go
@@ -1,7 +1,6 @@
 package arm
 
 import (
-	"fmt"
 	"proj/golite/ir"
 	st "proj/golite/symboltable"
 )
@@ -21,21 +20,23 @@ func TranslateToAssembly(funcfrags []*ir.FuncFrag, symTable *st.SymbolTable) []s
 		funcEntry := symTable.Contains(funcfrag.Label)
 		scopeSt := funcEntry.GetScopeST() // symbol table for the current scope
 		countVar := 0                     // count the variables inside this scope
-		for key, e := range scopeSt.HashTable() {
-			entry := *e
+		offset := 0
+		funcVarDict := make(map[string]int) // variable name -> offset, e.g. a -> -8, b -> -16
+		for varName, _ := range scopeSt.HashTable() {
+			//entry := *e
+			offset -= -8
+			funcVarDict[varName] = offset
 			countVar += 1
-			fmt.Println(key, entry.GetEntryType().GetName())
+			//fmt.Println(varName, entry.GetEntryType().GetName())
 		}
-		fmt.Println(countVar)
 
-		armInstructions = append(armInstructions, "\t.type " + funcfrag.Label + ",%function")
-		armInstructions = append(armInstructions, "\t.global " + funcfrag.Label)
+		armInstructions = append(armInstructions, "\t.type "+funcfrag.Label+",%function")
+		armInstructions = append(armInstructions, "\t.global "+funcfrag.Label)
 		armInstructions = append(armInstructions, "\t.p2align\t\t2")
 
 		for _, instruction := range funcfrag.Body {
 
 		}
-
 
 	}
 
