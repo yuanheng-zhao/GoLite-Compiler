8bba4391c254e7e7c50b761518c61ad49b62fad1
Ian An <qan001@uchicago.edu>
2021-12-10 09:39:50 -0600

    Assembly: storeref statement

--- proj/golite/ir/strRef.go
+++ proj/golite/ir/strRef.go
@@ -3,17 +3,19 @@ package ir
 import (
 	"bytes"
 	"fmt"
+	"proj/golite/utility"
 )
 
 // to access fields of a struct
 type StrRef struct {
-	target int
-	source int
-	field  string
+	target   int
+	source   int
+	field    string
+	fieldIdx int
 }
 
-func NewStrRef(target int, source int, field string) *StrRef {
-	return &StrRef{target, source, field}
+func NewStrRef(target int, source int, field string, fieldIdx int) *StrRef {
+	return &StrRef{target, source, field, fieldIdx}
 }
 
 func (instr *StrRef) GetTargets() []int {
@@ -51,7 +53,24 @@ func (instr *StrRef) String() string {
 }
 
 func (instr *StrRef) TranslateToAssembly(funcVarDict map[int]int, paramRegIds map[int]int) []string {
-	inst := []string{}
+	instruction := []string{}
 
-	return inst
+	targetRegId := utility.NextAvailReg()
+	targetOffSet := funcVarDict[instr.target]
+	instruction = append(instruction, fmt.Sprintf("\tldr x%v,[x29,#%v]",targetRegId,targetOffSet))
+
+	sourceRegId := utility.NextAvailReg()
+	sourceOffSet := funcVarDict[instr.source]
+	instruction = append(instruction, fmt.Sprintf("\tldr x%v,[x29,#%v]",sourceRegId,sourceOffSet))
+
+	fieldOffset := instr.fieldIdx * 8
+	instruction = append(instruction, fmt.Sprintf("\tstr x%v,[x%v,#]",sourceRegId,fieldOffset))
+	utility.ReleaseReg(targetRegId)
+	utility.ReleaseReg(sourceRegId)
+	return instruction
+
+	//ILOC:     strRef r15,r14,@x
+	//ldr x1,[x29,#-32]
+	//ldr x2,[x29,#-24]
+	//str x2, [x1,#0]
 }
